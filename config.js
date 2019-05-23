200d704dec31dd134d196c99cc688dbb6e506bd5
Sarah Burgess <sarahburgess287@gmail.com>
2019-05-22 20:14:44 -0700

    Added variables for JWT_SECRET and JWT_EXPIRY

--- config.js
+++ config.js
@@ -8,3 +8,5 @@ exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/hatchmydragon-test";
 
 exports.PORT = process.env.PORT || 3000;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
