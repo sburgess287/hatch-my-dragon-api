41eeddda0273573d9f1309a3bac0bcff1226d0ed
Sarah Burgess <sarahburgess287@gmail.com>
2019-05-26 14:26:09 -0700

    Updated local Url to http://localhost:8080

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 'use strict'
 
 exports.CLIENT_ORIGIN = 
-  process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+  process.env.CLIENT_ORIGIN || 'http://localhost:8080'
 exports.DATABASE_URL = 
   process.env.DATABASE_URL || "mongodb://localhost/hatchmydragon-app"
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/hatchmydragon-test";
 
-exports.PORT = process.env.PORT || 3000;
+exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
