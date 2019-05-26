70221bf028b585de270d0f941d15b672dcc05f2f
Sarah Burgess <sarahburgess287@gmail.com>
2019-05-25 17:53:48 -0700

    Installed jsonwebtoken, passport, passport-jwt, passport-local, created auth directory with associated index.js, router.js, strategies.js for authentication on login

---
+++ auth/router.js
@@ -0,0 +1,29 @@
+'use strict';
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const config = require('../config');
+const router = express.Router();
+
+const createAuthToken = function(user) {
+  return jwt.sign({user}, config.JWT_SECRET, {
+    subject: user.username,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
+const localAuth = passport.authenticate('local', {session: false});
+router.use(bodyParser.json());
+
+// User provides username/pw to login
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user.serialize());
+  res.json({authToken});
+});
+
+const jwtAuth = passport.authenticate('jwt', {session: false});
+
+module.exports = {router};
